2208c203f3779a6091d8089262db5efb7daae4df
tatugrigalashvili <146704405+tatugrigalashvili@users.noreply.github.com>
2024-05-17 13:56:05 +0400

    Add files via upload

---
+++ davaleba.js
@@ -0,0 +1,16 @@
+var cvladi = "dge";
+
+switch (cvladi) {
+    case "night":
+        window.alert("night");
+        break;
+    case "morning":
+        window.alert("morning");
+        break;
+    case "midnight":
+        window.alert("midnight");
+        break;
+    default:
+        window.alert("არასწორია")
+
+}
